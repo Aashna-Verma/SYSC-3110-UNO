2606ef50983a24ecc01daeaccb6f853c6f34fa73
Aashna-Verma <aashna.verma@outlook.com>
2023-10-16 23:23:42 -0400

    Card class + UML updated

---
+++ UNO/src/CardTest.java
@@ -0,0 +1,17 @@
+/**
+ * The test class for Card.
+ *
+ * @author  Aashna
+ * @version 1.0
+ */
+public class CardTest extends junit.framework.TestCase {
+
+    /**
+     * Sets up the test fixture.
+     * Called before every test case method.
+     */
+    @Override
+    protected void setUp() {
+
+    }
+}
---
+++ UNO/src/Colour.java
@@ -0,0 +1,15 @@
+/**
+ * The Colour enumerate
+ * Contains all the Colors the card can be
+ *
+ * @author  Aashna
+ * @version 1.0
+ */
+public enum Colour
+{
+    RED,
+    YELLOW,
+    GREEN,
+    BLUE,
+    WILD
+}
