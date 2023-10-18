0c3c7d5c13b3d7c1cf28c5790c36ee09439c78e6
Aashna-Verma <aashna.verma@outlook.com>
2023-10-18 13:56:39 -0400

    CardTest completed

--- UNO/src/DeckTest.java
+++ UNO/src/DeckTest.java
@@ -5,23 +5,15 @@
  * @version 1.0
  */
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class DeckTest {
-
-    @Test
+public class DeckTest extends junit.framework.TestCase{
     void populateDeck() {
         Deck deck = new Deck();
         deck.populateDeck();
     }
 
-    @Test
     void addCard() {
     }
 
-    @Test
     void getTopCard() {
     }
 }
