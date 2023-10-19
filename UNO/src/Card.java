da8fbfcf6e1a64dc2a94dd25a3449aa53cee6eb4
Aashna-Verma <aashna.verma@outlook.com>
2023-10-18 23:07:08 -0400

    added card score function

--- UNO/src/Card.java
+++ UNO/src/Card.java
@@ -5,6 +5,8 @@
  * @author  Aashna Verma 101225434
  * @version 1.0
  */
+import java.util.HashMap;
+
 public class Card {
     private final Value VALUE;
     private final Colour COLOUR;
@@ -36,6 +38,29 @@ public class Card {
      */
     public Colour getColour(){
         return this.COLOUR;
+    }
+
+    /**
+     * Gets the score related to the card
+     *
+     * @return an Int of the cards score
+     */
+    private int getScore(){
+        return switch (getValue()) {
+            case ONE -> 1;
+            case TWO -> 2;
+            case THREE -> 3;
+            case FOUR -> 4;
+            case FIVE -> 5;
+            case SIX -> 6;
+            case SEVEN -> 7;
+            case EIGHT -> 8;
+            case NINE -> 9;
+            case DRAW_ONE -> 10;
+            case WILD -> 40;
+            case WILD_DRAW_TWO -> 50;
+            default -> 20;
+        };
     }
 
     /**
