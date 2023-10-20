8eef446836eadf4d85263042f017326a9ee9ac2a
Angus Jull <angusjull1@gmail.com>
2023-10-19 21:24:29 -0400

    Fix bugs in main game loop
    Main game loop was passing wrong deck to players. Also changed how the rounds
    are printed and finished the main loop of play. Still need to add win condition

--- UNO/src/Game.java
+++ UNO/src/Game.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 /**
  * Game allows users to play a game of UNO Flip
  * 
@@ -18,80 +19,83 @@ public class Game {
         pile = new Deck();
         currentDeck.populateDeck();
     }
-    /**
-     * Checks if a player's play is a valid move
-     * 
-     * @param play the card that is being played on the deck
-     */
-    public boolean isValidMove(Card play) {
-        return false;
-    }
+
     /**
      * Plays a game of UNO Flip
      */
     public void playGame() {
         // Main game loop
-        while(true) { 
+        while (true) {
             int numPlayers = 0;
             do {
                 System.out.println("Enter the number of players (2-4)");
                 try {
                     numPlayers = scanner.nextInt();
-                }
-                catch (Exception e) {
+                    scanner.nextLine();
+                } catch (Exception e) {
                     numPlayers = -1;
                 }
             } while (numPlayers < 2 || numPlayers > 4);
-            
+
+            // Get player names and create the players
             for (int i = 0; i < numPlayers; i++) {
-                System.out.println("Enter a name for Player " + String.valueOf(i));
-                String name = scanner.nextLine();    
-                players.add(new Player(name));
+                System.out.print("Enter a name for Player " + String.valueOf(i) + ": ");
+                String name = scanner.nextLine();
+                // Create the player and give them their starting conditions
+                Player newPlayer = new Player(name);
+                players.add(newPlayer);
+                newPlayer.drawHand(currentDeck);
             }
 
             // Start the game
             int turnsTaken = 0;
             boolean gameOver = false;
             // Take a card off the deck to start
-            Card topCard = currentDeck.getTopCard(); 
+            Card topCard = currentDeck.getTopCard();
             do {
-
-                for (Player player: players) {
+                for (Player player : players) {
                     if (turnsTaken == 0) {
                         System.out.println("Starting Card: " + topCard);
-                    } 
-                    else {
-                        System.out.println("Played: " + topCard);    
+                    } else {
+                        System.out.println("Played: " + topCard);
                     }
                     printRoundInfo(topCard, player);
-
-                    // Have the player choose a card to play
-
-                    // Validate their move, add it back if it's not valid
                     
-                    // Change the top card to their move
+                    // Make a choice of what to play
+                    Card choice = null;
+                    do {
+                        choice = player.playCard(topCard, currentDeck);
+                        if (choice.validWith(topCard)) {
+                            topCard = choice;
+                        } else {
+                            System.out.println("Invalid move, try again");
+                            player.addCard(choice);
+                            choice = null;
+                        }
+                    } while (choice == null);
+                    topCard = choice;
 
                     // Run an action based on the card that was played
-                    
+
                     turnsTaken++;
                 }
             } while (!gameOver);
-
-
         }
     }
+
     /**
      * Print out information for the current Player
-     * @param played the card that was just played
+     * 
+     * @param played        the card that was just played
      * @param currentPlayer the player who's turn it is
      */
-    private void printRoundInfo (Card played, Player currentPlayer) {
+    private void printRoundInfo(Card played, Player currentPlayer) {
         System.out.println(currentPlayer.getName() + "'s Turn");
         System.out.println("Current Side: Light");
         System.out.println("Your cards:");
-        System.out.println(currentPlayer);
+        System.out.println(currentPlayer.handToString());
     }
-    
+
     public static void main(String[] args) {
         Game game = new Game();
         game.playGame();
