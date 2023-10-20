2599649e4c133fb88d1141688256ea0b12c212c4
darren <darrenwallace368@gmail.com>
2023-10-20 13:16:20 -0400

    Test cases

--- UNO/src/DeckTest.java
+++ UNO/src/DeckTest.java
@@ -6,17 +6,29 @@
  */
 
 public class DeckTest extends junit.framework.TestCase{
+    Deck emptyDeck, deck, deck2, deck3;
+    Card card, card2, card3;
+
+    protected void setUp() {
+        emptyDeck = new Deck();
+        deck = new Deck(); deck.populateDeck();
+        deck2 = new Deck(); deck2.populateDeck();
+        deck3 = new Deck(); deck3.populateDeck();
+        card = deck.getTopCard();
+        card2 = deck2.removeCard();
+        card3 = new Card(Value.FOUR, Colour.RED);
+    }
     public void test_populateDeck() {
-        Deck deck = new Deck();
-        deck.populateDeck();
-        assertNotNull(deck);
+        assertNull(emptyDeck.getTopCard());
+        assertNotSame(deck, deck2);
     }
 
     public void test_addCard() {
-
+        assertTrue(deck3.addCard(card3));
     }
 
     public void test_getTopCard() {
-
+        assertEquals(card, deck.getTopCard());
+        assertNotSame(card2, deck2.getTopCard());
     }
 }
