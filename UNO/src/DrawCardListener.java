93c6f00e849ed65517bcfb88abf538c2ebdd5e5b
Aashna-Verma <aashna.verma@outlook.com>
2023-11-14 21:47:44 -0500

    javadoc comments for the View

--- UNO/src/DrawCardListener.java
+++ UNO/src/DrawCardListener.java
@@ -1,11 +1,30 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * DrawCardListener is the ActionListener for Draw Card button
+ *
+ * @author Angus Jull
+ * @version 2.0
+ */
+
 public class DrawCardListener implements ActionListener {
     private Game game;
+
+    /**
+     * Constructor for DrawCardListener
+     *
+     * @param game the Game model
+     */
     public DrawCardListener(Game game) {
         this.game = game;
     }
+
+    /**
+     * The actionPerformed when an event occurs
+     *
+     * @param e the event to be processed
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         game.drawCard();
--- UNO/src/GameController.java
+++ UNO/src/GameController.java
@@ -1,3 +1,9 @@
+/**
+ * GameController initialize the setup of the UNO game and connects the model and the view for and interactive experience through a GUI
+ *
+ * @author Angus Jull
+ * @version 2.0
+ */
 public class GameController {
     private Game model;
     private GameView view;
@@ -5,8 +11,13 @@ public class GameController {
     private DrawCardListener drawListener;
     private NextPlayerListener nextListener;
 
+    /**
+     * constructor for GameController
+     *
+     * @param model the Game model
+     * @param view the GameView view
+     */
     public GameController(Game model, GameView view) {
-
         this.model = model;
         this.view = view;
         handListener = new HandListener(model);
@@ -20,6 +31,12 @@ public class GameController {
         // Add a view and update it to start the game
         model.addView(view);
     }
+
+    /**
+     * The main function that runs the game UNO
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Game game = new Game(GameView.viewPlayerCount());
         GameView view = new GameView(game);
