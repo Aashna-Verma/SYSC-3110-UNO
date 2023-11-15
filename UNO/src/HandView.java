e6dbd809fbcbe069272be23f2e76d004156fa749
AngusJull <angusjull1@gmail.com>
2023-11-14 19:14:56 -0500

    Disable hand after playing card

--- UNO/src/HandView.java
+++ UNO/src/HandView.java
@@ -1,28 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class HandView implements View {
-    Player player;
     private JPanel handPanel;
     private ActionListener listener; // Store the listener, because this object regularly deletes and creates new buttons
+    private ArrayList<JButton> cardButtons;
 
     public HandView(){
         handPanel = new JPanel(new GridLayout(1,0));
+        cardButtons = new ArrayList<>();
     }
     public JPanel getView(){
         return handPanel;
     }
-    public void newHand() {
-
-    }
 
     @Override
     public void update(Game game){
-        player = game.getCurrentPlayer();
+        Player player = game.getCurrentPlayer();
         handPanel.removeAll();
+        cardButtons.clear();
         for(int i = player.getNumCards()-1; i >= 0; i--) {
             JButton button = new JButton();
+            cardButtons.add(button);
             handPanel.add(button);
             Card card = player.getHand().get(i);
             // Cards are 1 indexed (from requirements)
@@ -33,6 +35,9 @@ public class HandView implements View {
         }
         //gameFrame.add(handPanel, BorderLayout.SOUTH);
         handPanel.updateUI();
+        for (JButton b: cardButtons) {
+            b.setEnabled(!game.isRoundOver());
+        }
     }
     /**
      * Add an action listener to this view for the card buttons
