59428925b0b2fba3d1cc521e7d6c6391b7cdf78e
btran <briantran@cmail.carleton.ca>
2023-11-24 13:38:01 -0500

    Commit for PR

--- UNO/src/Human.java
+++ UNO/src/Human.java
@@ -3,4 +3,14 @@ public class Human extends Player{
     public Human(String name){
         super(name);
     }
+
+    protected Card removeCard(int i){
+        try {
+            //card 0 is to pick up, however in the hand attribute 0 is the players' first card
+            return this.hand.remove(i - 1);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid card index\n");
+            return null;
+        }
+    }
 }
